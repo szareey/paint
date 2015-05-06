41d11b45d56d8118b88918433108716c0c6f1dc3
Sana <szareey@gmail.com>
2015-05-05 20:15:25 -0400

    Complete assignment

--- paint.rb
+++ paint.rb
@@ -1,1 +1,19 @@
-puts "my file"
+def calcCost(x, y, colours)
+  if colours <= 2
+    cost = 10
+  else
+    cost = 15
+  end
+  cost = (cost + x*y*15) * 1.15
+end
+
+
+
+puts 'What length of the sign?'
+signLength = gets.chomp.to_i
+puts 'What is the width of the sign?'
+signWidth = gets.chomp.to_i
+puts 'How many colours will you use?'
+colourCount = gets.chomp.to_i
+
+puts "The sign will cost $#{calcCost(signLength, signWidth, colourCount)}"
